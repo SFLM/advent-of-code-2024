eef85b1b5a44237db09a9bbe2f15a84774f87a97
Shan-Wei Mensing <SFLMensing@hotmail.com>
2025-11-11 14:22:06 +0100

    Day 3 part 1 done!

--- Day 03/solution.py
+++ Day 03/solution.py
@@ -1,13 +1,20 @@
+import re
+
 def main():
-    with open("example.txt") as f:
+    with open("input.txt") as f:
         raw = f.read()
     
     solution_1(raw)
 
-def solution_1(reports_input):
-    print()
+def solution_1(input):
+    total = 0
+    mul_instructions = re.findall("mul\((\d{1,3}),(\d{1,3})\)", input)
+    for mul_instruction in mul_instructions:
+        total += int(mul_instruction[0]) * int (mul_instruction[1])
+    
+    print(f"Solution 1: {total}")
 
-def solution_2(reports_input):
+def solution_2(input):
     print()
 
 if __name__ == "__main__":
