c9fdab1dc9b58ea18415de9768601fef0a6c2624
Shan-Wei Mensing <SFLMensing@hotmail.com>
2025-11-11 14:10:19 +0100

    Starting day 3!

---
+++ Day 03/solution.py
@@ -0,0 +1,14 @@
+def main():
+    with open("example.txt") as f:
+        raw = f.read()
+    
+    solution_1(raw)
+
+def solution_1(reports_input):
+    print()
+
+def solution_2(reports_input):
+    print()
+
+if __name__ == "__main__":
+    main()
