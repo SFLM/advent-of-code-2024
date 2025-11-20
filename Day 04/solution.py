640bae8fa247f61ad862ce8135f76f1fc7b1e8e0
Shan-Wei Mensing <SFLMensing@hotmail.com>
2025-11-20 16:23:54 +0100

    Starting day 4!

---
+++ Day 04/solution.py
@@ -0,0 +1,19 @@
+import re
+
+def main():
+    with open("input.txt") as f:
+        raw = f.read()
+    
+    solution1(raw)
+    solution2(raw)
+
+def solution1(input):
+    return
+    print(f"Solution 1: {total}")
+
+def solution2(input):
+    return
+    print(f"Solution 2: {total}")
+
+if __name__ == "__main__":
+    main()
