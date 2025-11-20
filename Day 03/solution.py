e9aceb2322bc08b454e6cae4b46c8e201c60cdc6
Shan-Wei Mensing <SFLMensing@hotmail.com>
2025-11-20 16:21:32 +0100

    Day 3 done!

--- Day 03/solution.py
+++ Day 03/solution.py
@@ -4,18 +4,30 @@ def main():
     with open("input.txt") as f:
         raw = f.read()
     
-    solution_1(raw)
+    solution1(raw)
+    solution2(raw)
 
-def solution_1(input):
+def solution1(input):
     total = 0
-    mul_instructions = re.findall("mul\((\d{1,3}),(\d{1,3})\)", input)
+    mul_instructions = re.findall(r"(?<=mul\()(\d{1,3}),(\d{1,3})(?=\))", input)
     for mul_instruction in mul_instructions:
         total += int(mul_instruction[0]) * int (mul_instruction[1])
     
     print(f"Solution 1: {total}")
 
-def solution_2(input):
-    print()
+def solution2(input):
+    total = 0
+    instructions = re.findall(r"(?<=mul\()\d{1,3},\d{1,3}(?=\))|do\(\)|don't\(\)", input)
+    enabled = True
+    for instruction in instructions:
+        if instruction == "do()":
+            enabled = True
+        elif instruction == "don't()":
+            enabled = False
+        elif enabled:
+            leftNumber, rightNumber = instruction.split(',')
+            total += int(leftNumber)*int(rightNumber)
+    print(f"Solution 2: {total}")
 
 if __name__ == "__main__":
     main()
