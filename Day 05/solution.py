876147d28200cf889cdcdbfb4ffdf103f90857d3
Shan-Wei Mensing <SFLMensing@hotmail.com>
2026-05-06 14:33:22 +0200

    Starting day 5!

---
+++ Day 05/solution.py
@@ -0,0 +1,14 @@
+def main():
+    with open("input.txt") as f:
+        raw = f.read().split()
+    
+    solution1(raw)
+    # solution2(raw)
+
+def solution1(input):
+    print()
+
+def solution2(input):
+    print()
+if __name__ == "__main__":
+    main()
