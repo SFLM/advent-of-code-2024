e14117b67dd77533551ee4b0d446150ee37c8b57
Shan-Wei Mensing <SFLMensing@hotmail.com>
2026-05-07 09:05:36 +0200

    Day 5 done!

--- Day 05/solution.py
+++ Day 05/solution.py
@@ -4,35 +4,57 @@ def main():
     
     page_ordering_rules, updates = raw
 
-    solution1(page_ordering_rules, updates)
-    # solution2(raw)
+    solution1(page_ordering_rules.split(), updates.split())
+    solution2(page_ordering_rules.split(), updates.split())
 
 def solution1(page_ordering_rules, updates):
-    total = 0
     rules_dict = create_rules_dict(page_ordering_rules)
-
-    for update_raw in updates.split():
-        update = update_raw.split(',')
-        error_found = False
-        for position, page in enumerate(update):
-            if page in rules_dict.keys():
-                if set(update[:position]) & set(rules_dict[page]): # Cool intersection
-                    error_found = True
-                    break
-        if not error_found:
-            total += int(update[len(update)//2])
-    
+    correct_updates = [update.split(',') for update in updates if not first_incorrect_position(rules_dict, update.split(','))]
+    total = sum(int(correct_update[len(correct_update)//2]) for correct_update in correct_updates)
     print(total)
 
-def solution2(input):
-    print()
+def solution2(page_ordering_rules, updates):
+    rules_dict = create_rules_dict(page_ordering_rules)
+    incorrect_updates = []
+    incorrect_positions = []
+    correct_updates = []
+    
+    for update in updates:
+        if incorrect_position := first_incorrect_position(rules_dict, update.split(',')):
+            incorrect_updates.append(update)
+            incorrect_positions.append(incorrect_position)
+    
+    for id, incorrect_update in enumerate(incorrect_updates):
+        correct_updates.append(recursive_fix_update(rules_dict, incorrect_update.split(','), incorrect_positions[id]))
+    
+    total = sum(int(correct_update[len(correct_update)//2]) for correct_update in correct_updates)
+
+    print(total)
 
 def create_rules_dict(input_rules):
     result = dict()
-    for rule in input_rules.split():
+    for rule in input_rules:
         rule_left, rule_right = rule.split('|')
         result.setdefault(rule_left, []).append(rule_right)
     return result
+
+def first_incorrect_position(rules_dict, update_input):
+    for position, page in enumerate(update_input):
+        if page in rules_dict.keys():
+            if set(update_input[:position]) & set(rules_dict[page]): # Cool intersection
+                return position
+    return False
+
+def recursive_fix_update(rules_dict, update, wrong_position):
+    if not wrong_position:
+        return update
+    pivot = update[wrong_position]
+    update.remove(pivot)
+    for insert_position, page in enumerate(update[:wrong_position]):
+        if page in rules_dict[pivot]:
+            new_update = update[:insert_position]+[pivot]+update[insert_position:]
+            return recursive_fix_update(rules_dict, new_update, first_incorrect_position(rules_dict, new_update))
+    
 
 if __name__ == "__main__":
     main()
