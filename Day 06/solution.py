3a91d72e70d4d5bcad79bf25cbd28c0ed4096974
Shan-Wei Mensing <SFLMensing@hotmail.com>
2026-05-07 09:08:51 +0200

    Starting day 6!

---
+++ Day 06/solution.py
@@ -0,0 +1,16 @@
+def main():
+    with open("input.txt") as f:
+        raw = f.read().split()
+    
+    solution1(raw)
+    solution2(raw)
+
+def solution1(page_ordering_rules, updates):
+    print()
+
+def solution2(page_ordering_rules, updates):
+    print()
+    
+
+if __name__ == "__main__":
+    main()
