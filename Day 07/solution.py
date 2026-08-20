eb5df1414c79c47b183ea339b0dcc36bcd4bb07b
Shan-Wei Mensing <s.mensing@hst.nl>
2026-08-20 14:06:47 +0200

    Day 7 done!

--- Day 07/solution.py
+++ Day 07/solution.py
@@ -20,19 +20,32 @@ def solution1(input):
 
 
 def solution2(input):
-    print()
+    total_calibration_result = 0
+    
+    for line in input:
+        test_value_raw, equation_numbers_raw = line.split(": ")
+        test_value = int(test_value_raw)
+        equation_numbers = [int(number) for number in equation_numbers_raw.split()]
+        if can_solve(test_value, equation_numbers, 1, equation_numbers[0], True):
+            total_calibration_result += test_value
+
+    print(f"Solution 2: {total_calibration_result}")
 
 
-def can_solve(test_value, equation_numbers, index, total):
+def can_solve(test_value, equation_numbers, index, total, concatenate=False):
     if (index == len(equation_numbers)):
         if (total == test_value):
             return True
         return False
 
-    if can_solve(test_value, equation_numbers, index+1, total+equation_numbers[index]):
+    if can_solve(test_value, equation_numbers, index+1, total+equation_numbers[index], concatenate):
         return True
     else:
-        return can_solve(test_value, equation_numbers, index+1, total*equation_numbers[index])
+        if can_solve(test_value, equation_numbers, index+1, total*equation_numbers[index], concatenate):
+            return True
+        if concatenate:
+            concatenated = str(total) + str(equation_numbers[index])
+            return can_solve(test_value, equation_numbers, index+1, int(concatenated), concatenate)
     
 
 
