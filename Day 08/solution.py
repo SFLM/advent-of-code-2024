0084a9e874acc4fbe6868271ec4f99f56cd47e78
Shan-Wei Mensing <s.mensing@hst.nl>
2026-08-20 14:08:46 +0200

    Starting day 8!

---
+++ Day 08/solution.py
@@ -0,0 +1,18 @@
+def main():
+    with open("input.txt") as f:
+        raw = f.read().splitlines()
+    
+    solution1(raw)
+    solution2(raw)
+
+
+def solution1(input):
+    print()
+
+
+def solution2(input):
+    print()    
+
+
+if __name__ == "__main__":
+    main()
