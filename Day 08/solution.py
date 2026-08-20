90d4b1dd76b600a7e5a60adde4bc82ec969a575a
Shan-Wei Mensing <s.mensing@hst.nl>
2026-08-20 16:32:00 +0200

    Day 8 done!

--- Day 08/solution.py
+++ Day 08/solution.py
@@ -1,3 +1,6 @@
+from math import gcd
+
+
 def main():
     with open("input.txt") as f:
         raw = f.read().splitlines()
@@ -17,7 +20,13 @@ def solution1(input):
 
 
 def solution2(input):
-    print()
+    input_height = len(input)
+    input_width = len(input[0])
+
+    antenna_locations = get_antenna_locations(input)
+    antinode_locations = get_antinode_locations(antenna_locations, input_height, input_width, resonant_harmonics=True)
+
+    print(f"Solution 2: {len(antinode_locations)}")
 
 
 def get_antenna_locations(input):
@@ -35,7 +44,7 @@ def get_antenna_locations(input):
     return result
 
 
-def get_antinode_locations(antenna_locations, map_height, map_width):
+def get_antinode_locations(antenna_locations, map_height, map_width, resonant_harmonics=False):
     antinode_locations = set()
 
     for frequency in antenna_locations:
@@ -43,16 +52,36 @@ def get_antinode_locations(antenna_locations, map_height, map_width):
             for other_location in antenna_locations[frequency]:
                 if location == other_location:
                     continue
-                row_difference = location[0] - other_location[0]
-                column_difference = location[1] - other_location[1]
-                antinodes_found = []
-                antinodes_found.append((location[0] + row_difference, location[1] + column_difference))
-                antinodes_found.append((other_location[0] - row_difference, other_location[1] - column_difference))
-                for antinode_location in antinodes_found:
-                    if (0 <= antinode_location[0] < map_height) and (0 <= antinode_location[1] < map_width):
-                        antinode_locations.add(antinode_location)
+                difference = (location[0] - other_location[0], location[1] - other_location[1])
+
+                if resonant_harmonics:
+                    greatest_common_denominator = gcd(difference[0], difference[1])
+                    difference = (difference[0] // greatest_common_denominator, difference[1] // greatest_common_denominator)
+                    if len(antenna_locations[frequency]) > 1:
+                        antinode_locations.update([location, other_location])
+
+                current_location = location
+                while True:
+                    antinode_found = find_antinode(current_location, difference, False)
+                    if (0 <= antinode_found[0] < map_height) and (0 <= antinode_found[1] < map_width):
+                        antinode_locations.add(antinode_found)
+                        if not resonant_harmonics:
+                            break
+                        current_location = antinode_found
+                    else:
+                        break
+                if not resonant_harmonics:
+                    antinode_found = find_antinode(other_location, difference, True)
+                    if (0 <= antinode_found[0] < map_height) and (0 <= antinode_found[1] < map_width):
+                        antinode_locations.add(antinode_found)
 
     return antinode_locations
+
+
+def find_antinode(anchor, difference, far_end):
+    if far_end:
+        return (anchor[0] - difference[0], anchor[1] - difference[1])
+    return (anchor[0] + difference[0], anchor[1] + difference[1])
 
 
 if __name__ == "__main__":
