223d1412d63a8d15b40d4180be616bda5e6fe66a
Shan-Wei Mensing <s.mensing@hst.nl>
2026-08-26 16:49:53 +0200

    Day 10 part 1 done!

--- Day 10/solution.py
+++ Day 10/solution.py
@@ -7,11 +7,47 @@ def main():
 
 
 def solution1(input):
+    result = 0
+    topographic_map = [tuple([int(height) for height in line]) for line in input]
+    for trailhead_location in get_trailheads(topographic_map):
+        trailhead_row, trailhead_column = trailhead_location
+        trailhead_set = trailhead_solver(topographic_map, trailhead_row, trailhead_column, set())
+        if trailhead_set != None:
+            result += len(trailhead_set)
+    print(f"Solution 1: {result}")
+
+
+def solution2(topographic_map):
     print()
 
 
-def solution2(input):
-    print()
+def get_trailheads(topographic_map):
+    trailheads = []
+    for row, line in enumerate(topographic_map):
+        for column, height in enumerate(line):
+            if height == 0:
+                trailheads.append((row,column))
+    return trailheads
+
+
+def trailhead_solver(topographic_map, row, column, found, last_value=-1):
+    if row<0 or row>=len(topographic_map) or column<0 or column>=len(topographic_map[0]):
+        return
+    
+    value = topographic_map[row][column]
+
+    if value != last_value+1:
+        return
+    if value == 9:
+        found.add((row,column))
+        return
+    
+    trailhead_solver(topographic_map, row, column-1, found, value)
+    trailhead_solver(topographic_map, row, column+1, found, value)
+    trailhead_solver(topographic_map, row+1, column, found, value)
+    trailhead_solver(topographic_map, row-1, column, found, value)
+
+    return found
 
 
 if __name__ == "__main__":
