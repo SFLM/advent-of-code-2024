03c7b78110ba46ed4e19675f5893629992eda92b
Shan-Wei Mensing <s.mensing@hst.nl>
2026-09-16 21:57:47 +0200

    Day 11 part 1 done!

--- Day 11/solution.py
+++ Day 11/solution.py
@@ -1,19 +1,42 @@
-from collections import defaultdict
+from collections import deque
 
 def main():
     with open("input.txt") as f:
-        raw = f.read().splitlines()
+        raw = f.read().split()
     
     solution1(raw)
     solution2(raw)
 
 
 def solution1(input):
-    print()
+    stones = deque(input)
+    print(len(blink(stones, 25)))
 
 
 def solution2(input):
     print()
+
+
+def blink(stones, iterations=1):
+    # print(stones)
+
+    post_blink_stones = deque()
+    while stones:
+        current_stone = int(stones.popleft())
+        if current_stone == 0:
+            post_blink_stones.append(1)
+            continue
+        if len(str(current_stone)) % 2 == 0:
+            half = int(len(str(current_stone))/2)
+            post_blink_stones.append(int(str(current_stone)[:half]))
+            post_blink_stones.append(int(str(current_stone)[half:]))
+            continue
+        post_blink_stones.append(current_stone*2024)
+
+    if iterations == 1:
+        return post_blink_stones
+
+    return blink(post_blink_stones, iterations-1)
 
 
 if __name__ == "__main__":
